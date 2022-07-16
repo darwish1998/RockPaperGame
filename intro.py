c9b4a1d903ca66f6ff25224a7476b66e7a058302
Ahmed <darwish144.ad@gmail.com>
2022-07-16 12:18:36 +0300

    Simple Game

---
+++ intro.py
@@ -0,0 +1,18 @@
+# print('My name is')
+# for i in range(5):
+#     print('Darwish Five Times (' + str(i) + ')')
+
+# import sys
+
+
+# while True:
+#     print("Type exit to quit")
+
+#     response = input()
+#     if response == 'exit':
+#         sys.exit()
+#     print('You type ' + response + ' .')
+
+
+
+
